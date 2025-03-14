5694cae2a5499b51b25361edd918962852ba84b4
Rieltzx25 <80047117+Rieltzx25@users.noreply.github.com>
2025-03-15 00:22:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,8 +169,8 @@ FEATURES = ["Fear and Greed Index", "Bitcoin Rainbow Chart"]
 # --------------------------------------
 # Sidebar: Pilih Section dengan Logo
 # --------------------------------------
-# Tambahkan logo dari direktori utama
-st.sidebar.image("cat.logo.webp", use_column_width=False, width=100)
+# Tambahkan logo dari direktori utama dengan parameter yang diperbarui
+st.sidebar.image("cat.logo.webp", use_container_width=False, width=100)
 st.sidebar.header("Navigation")
 section = st.sidebar.radio("Choose Section", ["News Feed", "Features"])
 
